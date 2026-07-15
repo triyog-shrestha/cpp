86a2d2036c965a720c8c6cc907203e1d6993e1c0
triyog-shrestha <triyogshrestha86@gmail.com>
2026-07-15 23:34:30 +0545

    made a piece fall in the tetris

--- tetrisBoard.cpp
+++ tetrisBoard.cpp
@@ -1,15 +1,22 @@
+#include <cstdlib>
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 #include <vector>
 
 void makeGrid(const vector<vector<int>>& board){
     for (const auto& row:board){
         for (int tile:row){
-            cout<<(tile ? '#' : '.');
+            switch (tile) {
+                case 1: cout<<'#';break;
+                case 0: cout<<'.';break;
+                case 2: cout<<'=';break;
+            
+            }
             cout<<'\t';
         }
         cout<<'\n';
-        cout<<'\v';
     }
 
 }
@@ -39,5 +46,22 @@ int main(){
     };
 
     makeGrid(board);
-    
+    for (int i = 0; i<3; ++i){
+        int xp = 1;
+        int yp = 1;
+
+        board[xp][yp] = 2;
+
+        while (xp < board.size() - 2) {
+            system("clear");
+
+            makeGrid(board);
+
+            this_thread::sleep_for(chrono::milliseconds(500));
+
+            board[xp][yp] = 0;
+            xp++;
+            board[xp][yp] = 2;
+        }
+    }
 }
